3e02d379161400d734e1846e866005cf8e4b7268
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-02-01 20:48:15 +0300

    Добавьте файлы проекта.

---
+++ a/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a + " - вот какое число Вы ввели.");
+    }
+}
