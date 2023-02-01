a4257dce7a901b295d5199eb7144be590dabc488
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-02-01 21:09:36 +0300

    изм

--- a/Program.cs
+++ a/Program.cs
@@ -5,6 +5,7 @@ public class MainClass
 {
     public static void Main()
     {
+        Console.WriteLine("Введите число");
         int a = int.Parse(Console.ReadLine());
         Console.WriteLine(a + " - вот какое число Вы ввели.");
     }
